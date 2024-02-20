aeb4d93216356c6b998e1773e74231076d69fa90
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-20 20:57:54 +0900

    - application module 추가

---
+++ app/src/main/java/com/gyleedev/githubsearch/GithubSearchApplication.kt
@@ -0,0 +1,10 @@
+package com.gyleedev.githubsearch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GithubSearchApplication : Application() {
+
+
+}
