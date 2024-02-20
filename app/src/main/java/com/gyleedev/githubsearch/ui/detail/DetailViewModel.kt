66ae564d19a2802e2651633b04977bf171252d58
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-20 22:11:42 +0900

    - viewModel 추가

---
+++ app/src/main/java/com/gyleedev/githubsearch/ui/detail/DetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.gyleedev.githubsearch.ui.detail
+
+import com.gyleedev.githubsearch.core.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+
+): BaseViewModel() {
+}
---
+++ app/src/main/java/com/gyleedev/githubsearch/ui/home/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.gyleedev.githubsearch.ui.home
+
+import com.gyleedev.githubsearch.core.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+
+): BaseViewModel() {
+}
