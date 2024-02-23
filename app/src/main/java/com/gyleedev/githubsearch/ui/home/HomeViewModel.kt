9823cbaa0930ce47015c149e5f509cea26a68786
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-23 23:43:27 +0900

    - HomeViewModel에 UserList가져오는 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/home/HomeViewModel.kt
@@ -1,11 +1,18 @@
 package com.gyleedev.githubsearch.ui.home
 
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.gyleedev.githubsearch.core.BaseViewModel
+import com.gyleedev.githubsearch.domain.model.UserModel
+import com.gyleedev.githubsearch.domain.usecase.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-
-): BaseViewModel() {
+    private val useCase: GetUserUseCase
+) : BaseViewModel() {
+    fun getUsers(): Flow<PagingData<UserModel>> = useCase.getUsers().cachedIn(viewModelScope)
 }
