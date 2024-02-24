b8fd70395f96a0c62521470d92fc87b7b0000078
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-24 23:12:42 +0900

    - homeViewModel에 필요한 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/home/HomeViewModel.kt
@@ -9,10 +9,34 @@ import com.gyleedev.githubsearch.domain.usecase.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val useCase: GetUserUseCase
 ) : BaseViewModel() {
+    private val _searchId = MutableStateFlow("")
+
+    private val _userInfo = MutableStateFlow<UserModel?>(null)
+    val userInfo: StateFlow<UserModel?> = _userInfo
     fun getUsers(): Flow<PagingData<UserModel>> = useCase.getUsers().cachedIn(viewModelScope)
+    fun getUser() {
+        viewModelScope.launch {
+            _userInfo.emit(useCase.getUserAtHome(_searchId.value))
+        }
+    }
+
+    fun updateSearchId(id: String) {
+        viewModelScope.launch {
+            _searchId.emit(id)
+        }
+    }
+
+    fun resetUser() {
+        viewModelScope.launch {
+            _userInfo.emit(null)
+        }
+    }
 }
