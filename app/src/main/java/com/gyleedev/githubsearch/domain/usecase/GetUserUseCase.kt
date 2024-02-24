eefa57ab514d300f1c66d252c001ea9fb6c5ebca
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-24 23:01:57 +0900

    - getUserUseCase에 home용 user가져오는 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/domain/usecase/GetUserUseCase.kt
+++ app/src/main/java/com/gyleedev/githubsearch/domain/usecase/GetUserUseCase.kt
@@ -1,11 +1,14 @@
 package com.gyleedev.githubsearch.domain.usecase
 
 import com.gyleedev.githubsearch.data.repository.GitHubRepository
+import com.gyleedev.githubsearch.domain.model.UserModel
 import javax.inject.Inject
 
 class GetUserUseCase @Inject constructor(
     private val repository: GitHubRepository
 ) {
-    suspend fun execute(user: String) = repository.getUser(user)
+    suspend fun execute(user: String): UserModel? = repository.getUser(user)
     fun getUsers() = repository.getUsers()
+
+    suspend fun getUserAtHome(user: String): UserModel? = repository.getUserAtHome(user)
 }
