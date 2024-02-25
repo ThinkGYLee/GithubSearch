ee495fab8273f54197d133474a5ca43fed7a74f6
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-25 14:53:57 +0900

    - DetailScreen구현

---
+++ app/src/main/java/com/gyleedev/githubsearch/ui/detail/DetailScreen.kt
@@ -0,0 +1,324 @@
+package com.gyleedev.githubsearch.ui.detail
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Apartment
+import androidx.compose.material.icons.filled.Link
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.gyleedev.githubsearch.R
+import com.gyleedev.githubsearch.domain.model.DetailFeed
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(viewModel: DetailViewModel = hiltViewModel(), modifier: Modifier) {
+
+    val list = viewModel.itemList.collectAsStateWithLifecycle()
+    println("screen")
+    println(list.value)
+    println("screen")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "") },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "back"
+                    )
+                },
+                modifier = Modifier
+            )
+
+        },
+        modifier = modifier
+            .fillMaxSize()
+            .padding(4.dp)
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(it)
+                .padding(vertical = 4.dp)
+        ) {
+            items(list.value.size) { current ->
+                when (list.value[current]) {
+                    is DetailFeed.UserProfile -> {
+                        println("userProfile")
+                        DetailUserTitleItem(list.value[current] as DetailFeed.UserProfile)
+                    }
+
+                    is DetailFeed.UserDetail -> {
+                        println("userDetail")
+                        DetailUserInfoItem(user = list.value[current] as DetailFeed.UserDetail)
+                    }
+
+                    is DetailFeed.RepoTitle -> {
+                        println("RepoTitle")
+                        DetailRepoTitle()
+                    }
+
+                    is DetailFeed.RepoDetail -> {
+                        println("RepoDetail")
+                        DetailRepoItem(repos = list.value[current] as DetailFeed.RepoDetail)
+                    }
+
+                    else -> {
+                        println("else")
+                        DetailRepoNoItem()
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+private fun DetailUserTitleItem(user: DetailFeed.UserProfile) {
+    val data = user.userModel
+    println("data $data")
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            GlideImage(
+                model = (data.avatar),
+                contentDescription = null
+            )
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = data.repos.toString(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "repos",
+                )
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = data.followers.toString(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "followers",
+                )
+            }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = data.following.toString(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "following",
+                )
+            }
+
+        }
+    }
+}
+
+
+@Composable
+private fun DetailUserInfoItem(user: DetailFeed.UserDetail) {
+    val data = user.userModel
+    println("data $data")
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+
+        Column(modifier = Modifier.padding(12.dp)) {
+            data.name?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(12.dp)
+                )
+            }
+            Text(
+                text = data.login,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(12.dp)
+            )
+            data.bio?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(12.dp)
+                )
+            }
+
+            if (data.company != null) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Apartment,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(24.dp)
+                            .height(24.dp)
+                    )
+                    Text(text = data.company)
+                }
+            }
+
+            if (data.email != null) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Mail,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(24.dp)
+                            .height(24.dp)
+                    )
+                    Text(text = data.email)
+                }
+            }
+
+            if (data.blogUrl != "") {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Link,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(24.dp)
+                            .height(24.dp)
+                    )
+                    data.blogUrl?.let { Text(text = it) }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DetailRepoTitle() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+        Text(
+            text = "Repository",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp)
+        )
+    }
+}
+
+@Composable
+private fun DetailRepoItem(repos: DetailFeed.RepoDetail) {
+    val data = repos.repositoryModel
+    println("data $data")
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        Column(modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp)) {
+            data.name?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+            data.description?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.padding(vertical = 4.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .width(24.dp)
+                        .height(24.dp),
+                    colorResource(id = R.color.yellow)
+                )
+                Text(text = data.stargazer.toString(), modifier = Modifier.padding(8.dp))
+                data.language?.let { Text(text = it, modifier = Modifier.padding(8.dp)) }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DetailRepoNoItem() {
+
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "No Repository",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp, vertical = 12.dp),
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+}
