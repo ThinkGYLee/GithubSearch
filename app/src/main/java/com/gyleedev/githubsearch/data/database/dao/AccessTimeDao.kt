454faa7ce8e6fcd2f962de34071727d2e292e582
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-25 15:18:03 +0900

    - updateTime함수 추가 - 대소문자 구분 없애는 코드 추가

--- app/src/main/java/com/gyleedev/githubsearch/data/database/dao/AccessTimeDao.kt
+++ app/src/main/java/com/gyleedev/githubsearch/data/database/dao/AccessTimeDao.kt
@@ -4,12 +4,13 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.gyleedev.githubsearch.data.database.entity.AccessTime
 
 
 @Dao
 interface AccessTimeDao {
-    @Query("SELECT * FROM access_time WHERE github_id = :id")
+    @Query("SELECT * FROM access_time WHERE github_id = :id  COLLATE NOCASE")
     fun getTimeByGithubId(id: String): AccessTime
 
     @Insert
@@ -17,4 +18,7 @@ interface AccessTimeDao {
 
     @Delete
     fun deleteTime(accessTime: AccessTime)
+
+    @Update
+    fun updateTime(accessTime: AccessTime)
 }
