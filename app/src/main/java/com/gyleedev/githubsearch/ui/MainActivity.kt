c52b652f9495bff38181f0b2cbca3155440c9a64
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-25 14:43:05 +0900

    - 메인 액티비티에서 사용하지 않는 함수 제거

--- app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
@@ -3,10 +3,6 @@ package com.gyleedev.githubsearch.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.gyleedev.githubsearch.ui.theme.GithubSearchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,21 +16,5 @@ class MainActivity : ComponentActivity() {
                 GithubSearchApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GithubSearchTheme {
-        Greeting("Android")
     }
 }
