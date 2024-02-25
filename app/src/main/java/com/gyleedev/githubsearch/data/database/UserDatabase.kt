f1d67670e4104f8d9f91875674aec139bfe747c6
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-25 14:51:20 +0900

    - dbTypeConverter를 database에 적용

--- app/src/main/java/com/gyleedev/githubsearch/data/database/UserDatabase.kt
+++ app/src/main/java/com/gyleedev/githubsearch/data/database/UserDatabase.kt
@@ -2,12 +2,13 @@ package com.gyleedev.githubsearch.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.gyleedev.githubsearch.data.database.dao.AccessTimeDao
 import com.gyleedev.githubsearch.data.database.dao.ReposDao
 import com.gyleedev.githubsearch.data.database.dao.UserDao
+import com.gyleedev.githubsearch.data.database.entity.AccessTime
 import com.gyleedev.githubsearch.data.database.entity.ReposEntity
 import com.gyleedev.githubsearch.data.database.entity.UserEntity
-import com.gyleedev.githubsearch.data.database.entity.AccessTime
 
 @Database(
     entities = [
@@ -19,6 +20,7 @@ import com.gyleedev.githubsearch.data.database.entity.AccessTime
     exportSchema = true
 )
 
+@TypeConverters(DbTypeConverters::class)
 abstract class UserDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun reposDao(): ReposDao
