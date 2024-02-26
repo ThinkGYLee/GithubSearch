65d3954b2f97aab8be4512330048eced7ac8bd66
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-26 23:06:23 +0900

    - BottomNavigation 추가

--- app/src/main/java/com/gyleedev/githubsearch/ui/GithubSearchScreen.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/GithubSearchScreen.kt
@@ -1,12 +1,27 @@
 package com.gyleedev.githubsearch.ui
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Details
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -18,10 +33,14 @@ import com.gyleedev.githubsearch.R
 import com.gyleedev.githubsearch.ui.detail.DetailScreen
 import com.gyleedev.githubsearch.ui.home.HomeScreen
 
-enum class GithubSearchScreen(@StringRes val title: Int) {
-    Home(title = R.string.app_name),
-    Detail(title = R.string.title_detail),
-    Favorite(title = R.string.title_favorite),
+
+sealed class BottomNavItem(
+    val title: Int, val icons: ImageVector, val screenRoute: String
+) {
+    data object Home : BottomNavItem(R.string.app_name, Icons.Filled.Home, HOME)
+    data object Detail : BottomNavItem(R.string.title_detail, Icons.Filled.Details, DETAIL)
+    data object Setting : BottomNavItem(R.string.title_setting, Icons.Filled.Settings, SETTING)
+    data object Favorite : BottomNavItem(R.string.title_favorite, Icons.Filled.StarBorder, FAVORITE)
 }
 
 
@@ -31,26 +50,30 @@ fun GithubSearchApp(
 ) {
 
     val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry?.destination?.route
 
     Scaffold(
+        bottomBar = {
+            BottomNavigation(navController = navController)
+        }
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = GithubSearchScreen.Home.name,
+            startDestination = BottomNavItem.Home.screenRoute,
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable(route = GithubSearchScreen.Home.name) {
+            composable(route = BottomNavItem.Home.screenRoute) {
                 HomeScreen(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(),
-                    moveToDetail = { navController.navigate("${GithubSearchScreen.Detail.name}/$it") },
+                    moveToDetail = { navController.navigate("${BottomNavItem.Detail.screenRoute}/$it") },
                 )
             }
 
 
             composable(
-                route = "${GithubSearchScreen.Detail.name}/{id}",
+                route = "${BottomNavItem.Detail.screenRoute}/{id}",
                 arguments = listOf(
                     navArgument("id") {
                         type = NavType.StringType
@@ -64,10 +87,51 @@ fun GithubSearchApp(
                         .padding()
                 )
             }
-
-
-
         }
+    }
+}
 
+@Composable
+fun BottomNavigation(navController: NavHostController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Favorite,
+        BottomNavItem.Setting,
+    )
+
+    androidx.compose.material.BottomNavigation(
+        backgroundColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icons,
+                        contentDescription = stringResource(id = item.title),
+                        modifier = Modifier
+                            .width(26.dp)
+                            .height(26.dp)
+                    )
+                },
+                label = { Text(stringResource(id = item.title), fontSize = 9.sp) },
+                selectedContentColor = MaterialTheme.colorScheme.primary,
+                unselectedContentColor = Gray,
+                selected = currentRoute == item.screenRoute,
+                alwaysShowLabel = false,
+                onClick = {
+                    navController.navigate(item.screenRoute) {
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(it) { saveState = true }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
     }
 }
