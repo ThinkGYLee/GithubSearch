2247bcad1949cfe225eace6d8b55815ab1255722
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-28 07:22:21 +0900

    - favoriteViewModel에 updateStatus 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/favorite/FavoriteViewModel.kt
@@ -7,12 +7,20 @@ import com.gyleedev.githubsearch.core.BaseViewModel
 import com.gyleedev.githubsearch.domain.model.UserModel
 import com.gyleedev.githubsearch.domain.usecase.FavoriteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteViewModel @Inject constructor(
     private val useCase: FavoriteUseCase
-): BaseViewModel(){
+) : BaseViewModel() {
+
     fun getUsers(): Flow<PagingData<UserModel>> = useCase.getFavorites().cachedIn(viewModelScope)
+
+    fun updateFavoriteStatus(user: UserModel) {
+        viewModelScope.launch {
+            useCase.update(user.login)
+        }
+    }
 }
