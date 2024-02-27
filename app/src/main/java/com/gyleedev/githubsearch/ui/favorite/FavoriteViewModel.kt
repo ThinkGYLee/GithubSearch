bb6a469e75bfe2cac4e9292d57e0ce41a1804fde
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-28 08:55:08 +0900

    - user 구조 변경, 필터링용 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/favorite/FavoriteViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.gyleedev.githubsearch.core.BaseViewModel
+import com.gyleedev.githubsearch.domain.model.FilterStatus
 import com.gyleedev.githubsearch.domain.model.UserModel
 import com.gyleedev.githubsearch.domain.usecase.FavoriteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,7 +17,26 @@ class FavoriteViewModel @Inject constructor(
     private val useCase: FavoriteUseCase
 ) : BaseViewModel() {
 
-    fun getUsers(): Flow<PagingData<UserModel>> = useCase.getFavorites().cachedIn(viewModelScope)
+    var user: Flow<PagingData<UserModel>>
+
+    init {
+        user = getUsers()
+    }
+
+    private fun getUsers(): Flow<PagingData<UserModel>> =
+        useCase.getFavorites(FilterStatus.ALL).cachedIn(viewModelScope)
+
+    fun userFilterAll() {
+        user = getUsers()
+    }
+
+    fun userFilterHasRepos() {
+        user = useCase.getFavorites(FilterStatus.REPO).cachedIn(viewModelScope)
+    }
+
+    fun userFilterNoRepos() {
+        user = useCase.getFavorites(FilterStatus.NOREPO).cachedIn(viewModelScope)
+    }
 
     fun updateFavoriteStatus(user: UserModel) {
         viewModelScope.launch {
