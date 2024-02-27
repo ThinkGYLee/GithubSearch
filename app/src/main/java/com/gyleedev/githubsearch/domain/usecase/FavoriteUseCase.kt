49a3b0a6d2ec12adadb87eddcd7c47953cc9751f
ThinkGYLee <ntnpholicfp@gmail.com>
2024-02-28 07:21:57 +0900

    - favoriteUseCase에 업데이트 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/domain/usecase/FavoriteUseCase.kt
+++ app/src/main/java/com/gyleedev/githubsearch/domain/usecase/FavoriteUseCase.kt
@@ -1,10 +1,18 @@
 package com.gyleedev.githubsearch.domain.usecase
 
 import com.gyleedev.githubsearch.data.repository.GitHubRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class FavoriteUseCase @Inject constructor(
     private val repository: GitHubRepository
 ) {
     fun getFavorites() = repository.getFavorites()
+
+    suspend fun update(id: String) {
+        return withContext(Dispatchers.IO) {
+            repository.updateUser(id)
+        }
+    }
 }
