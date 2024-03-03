b904f537587a329c38584ba37b6b7cdc42aec3f5
ThinkGYLee <ntnpholicfp@gmail.com>
2024-03-03 20:44:44 +0900

    - mainActivity 린트 수정

--- app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.gyleedev.githubsearch.ui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresExtension
 import com.gyleedev.githubsearch.ui.theme.GithubSearchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GithubSearchTheme {
-                // A surface container using the 'background' color from the theme
                 GithubSearchApp()
             }
         }
