1f6b6f0dc6b07af9f413f40e69c961d1e896a7dd
ThinkGYLee <ntnpholicfp@gmail.com>
2024-03-18 23:25:29 +0900

    - userDao에 함수 추가

--- app/src/main/java/com/gyleedev/githubsearch/data/database/dao/UserDao.kt
+++ app/src/main/java/com/gyleedev/githubsearch/data/database/dao/UserDao.kt
@@ -1,16 +1,27 @@
 package com.gyleedev.githubsearch.data.database.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.gyleedev.githubsearch.data.database.entity.UserEntity
+import com.gyleedev.githubsearch.domain.model.FilterStatus
 
 @Dao
 interface UserDao {
 
     @Query("SELECT * FROM user LIMIT 10 OFFSET (:page-1)*10")
     fun getUsers(page: Int): List<UserEntity>
+
+    @Query("SELECT * FROM user ")
+    fun getUsers(): PagingSource<Int, UserEntity>
+
+    @Query("SELECT * FROM user WHERE repos >0")
+    fun getUsersRepo(): PagingSource<Int, UserEntity>
+
+    @Query("SELECT * FROM user WHERE repos =0")
+    fun getUsersNonRepo(): PagingSource<Int, UserEntity>
 
     @Query("SELECT * FROM user WHERE favorite = :favorite LIMIT 10 OFFSET (:page-1)*10")
     fun getFavorite(page: Int, favorite: Boolean = true): List<UserEntity>
@@ -26,4 +37,12 @@ interface UserDao {
 
     @Update
     fun updateUser(user: UserEntity)
+
+    fun getUsers(status: FilterStatus): PagingSource<Int,UserEntity> {
+       return when (status) {
+            FilterStatus.ALL -> getUsers()
+            FilterStatus.REPO -> getUsersRepo()
+            FilterStatus.NOREPO -> getUsersNonRepo()
+        }
+    }
 }
