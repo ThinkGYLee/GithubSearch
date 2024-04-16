02a208b9390b9d5fe1b925e168311770d5494c47
ThinkGYLee <ntnpholicfp@gmail.com>
2024-04-16 23:16:36 +0900

    - NetworkModule에서 qualifier로 구분

--- app/src/main/java/com/gyleedev/githubsearch/data/remote/NetworkModule.kt
+++ app/src/main/java/com/gyleedev/githubsearch/data/remote/NetworkModule.kt
@@ -10,17 +10,30 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class TypeAccess
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class TypeApi
+
     @Provides
-    fun provideBaseUrl() = "https://api.github.com"
+    fun provideBaseApiUrl() = "https://api.github.com"
+
+    @Provides
+    fun provideBaseAccessUrl() = "https://github.com"
 
     @Singleton
     @Provides
-    fun provideOkHttpClient(): OkHttpClient = if (BuildConfig.DEBUG) {
+    @TypeApi
+    fun provideApiOkHttpClient(): OkHttpClient = if (BuildConfig.DEBUG) {
         val loggingInterceptor = HttpLoggingInterceptor()
         val tokenInterceptor = TokenInterceptor()
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -34,17 +47,52 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    @TypeApi
+    fun provideApiRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .client(okHttpClient)
-            .baseUrl(provideBaseUrl())
+            .baseUrl(provideBaseApiUrl())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     @Singleton
     @Provides
-    fun provideGithubApi(retrofit: Retrofit): GithubApiService {
+    @TypeApi
+    fun provideApiGithubApi(retrofit: Retrofit): GithubApiService {
+        return retrofit.create(GithubApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    @TypeAccess
+    fun provideAccessOkHttpClient(): OkHttpClient = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        val tokenInterceptor = TokenInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(tokenInterceptor)
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+    } else {
+        OkHttpClient.Builder().build()
+    }
+
+    @Singleton
+    @Provides
+    @TypeAccess
+    fun provideAccessRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(provideBaseApiUrl())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @TypeAccess
+    fun provideAccessGithubApi(retrofit: Retrofit): GithubApiService {
         return retrofit.create(GithubApiService::class.java)
     }
 
