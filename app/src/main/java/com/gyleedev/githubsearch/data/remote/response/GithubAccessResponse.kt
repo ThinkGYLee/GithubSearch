20fb9a70bf0b2f1c43108237f410c86cc2b10c31
ThinkGYLee <ntnpholicfp@gmail.com>
2024-04-19 21:25:28 +0900

    - GithubAccessResponse에서 토큰정보가 들어가는 것으로 수정

--- app/src/main/java/com/gyleedev/githubsearch/data/remote/response/GithubAccessResponse.kt
+++ app/src/main/java/com/gyleedev/githubsearch/data/remote/response/GithubAccessResponse.kt
@@ -1,23 +1,7 @@
 package com.gyleedev.githubsearch.data.remote.response
 
 import com.google.gson.annotations.SerializedName
-import com.gyleedev.githubsearch.domain.model.RepositoryModel
-import retrofit2.http.Field
-import java.io.Serial
 
 data class GithubAccessResponse(
-    @SerializedName("client_id") val clientId: String,
-    @SerializedName("client_secret") val clientSecret: String,
-    @SerializedName("code") val code: String
+    @SerializedName("access_token") val accessToken: String
 )
-/*
-fun GithubAccessResponse.toModel(id: String): RepositoryModel {
-    return RepositoryModel(
-        name = name,
-        userGithubId = id,
-        description = description,
-        language = language,
-        stargazer = stargazer,
-        favorite = false
-    )
-}*/
