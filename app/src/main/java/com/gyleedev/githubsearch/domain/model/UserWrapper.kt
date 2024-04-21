f55a94bbf732b55c02cb3d9f40398cdfd881dc9b
ThinkGYLee <ntnpholicfp@gmail.com>
2024-04-21 22:54:32 +0900

    - userWrapper 클래스 생성

---
+++ app/src/main/java/com/gyleedev/githubsearch/domain/model/UserWrapper.kt
@@ -0,0 +1,16 @@
+package com.gyleedev.githubsearch.domain.model
+
+sealed interface UserWrapper {
+    data class FromDatabase(
+        val data: UserModel
+    ): UserWrapper
+
+    data class Success(
+        val status: SearchStatus,
+        val data: UserModel
+    ): UserWrapper
+
+    data class Failure(
+        val status: SearchStatus
+    ): UserWrapper
+}
