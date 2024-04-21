87f7da65c16a7958eaae2705cf0a69b5e8a919e0
ThinkGYLee <ntnpholicfp@gmail.com>
2024-04-21 22:44:56 +0900

    - PreferenceUtil 변경

--- app/src/main/java/com/gyleedev/githubsearch/util/PreferenceUtil.kt
+++ app/src/main/java/com/gyleedev/githubsearch/util/PreferenceUtil.kt
@@ -4,19 +4,21 @@ import android.content.Context
 import android.content.SharedPreferences
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-class PreferenceUtil(context: Context) {
-    private val prefs: SharedPreferences =
+class PreferenceUtil(@ApplicationContext context: Context) {
+
+    private val accessPreference: SharedPreferences =
         context.getSharedPreferences("AccessToken", Context.MODE_PRIVATE)
 
     fun getString(key: String = "Token", defValue: String): String {
-        return prefs.getString(key, defValue) as String
+        return accessPreference.getString(key, defValue) as String
     }
 
     fun setString(key: String = "Token", str: String) {
-        prefs.edit().putString(key, str).apply()
+        accessPreference.edit().putString(key, str).apply()
     }
 }
