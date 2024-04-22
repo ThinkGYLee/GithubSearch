ae51b54df9023d8f072f8eb958e6728fe706e4d9
ThinkGYLee <ntnpholicfp@gmail.com>
2024-04-22 22:50:09 +0900

    - HomeSearchUserUseCase의 리턴타입 변경

--- app/src/main/java/com/gyleedev/githubsearch/domain/usecase/HomeSearchUserUseCase.kt
+++ app/src/main/java/com/gyleedev/githubsearch/domain/usecase/HomeSearchUserUseCase.kt
@@ -1,11 +1,13 @@
 package com.gyleedev.githubsearch.domain.usecase
 
 import com.gyleedev.githubsearch.data.repository.GitHubRepository
-import com.gyleedev.githubsearch.domain.model.UserModel
+import com.gyleedev.githubsearch.domain.model.UserWrapper
 import javax.inject.Inject
 
 class HomeSearchUserUseCase @Inject constructor(
     private val repository: GitHubRepository
 ) {
-    suspend operator fun invoke(user: String): UserModel? = repository.getUserAtHome(user)
+    suspend operator fun invoke(user: String): UserWrapper {
+        return repository.getUserAtHome(user)
+    }
 }
