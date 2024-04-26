a80c54a0413f3a9c0f72500b41f5df28493dd397
ThinkGYLee <ntnpholicfp@gmail.com>
2024-04-26 23:08:44 +0900

    - detail 관련 useCase 내부 로직 변경

--- app/src/main/java/com/gyleedev/githubsearch/domain/usecase/DetailUpdateFavoriteStatusUseCase.kt
+++ app/src/main/java/com/gyleedev/githubsearch/domain/usecase/DetailUpdateFavoriteStatusUseCase.kt
@@ -2,6 +2,7 @@ package com.gyleedev.githubsearch.domain.usecase
 
 import com.gyleedev.githubsearch.data.repository.GitHubRepository
 import com.gyleedev.githubsearch.domain.model.DetailFeed
+import com.gyleedev.githubsearch.domain.model.UserWrapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -14,7 +15,12 @@ class DetailUpdateFavoriteStatusUseCase @Inject constructor(
         return withContext(Dispatchers.IO) {
             val user = repository.updateUserFavorite(id)
             val repo = repository.getReposFromDatabase(id)
-            ModelToFeed.modelToFeed(user, repo)
+            val userModel = if(user is UserWrapper.FromDatabase) {
+                user.data
+            } else {
+                null
+            }
+            ModelToFeed.modelToFeed(userModel, repo)
         }
     }
 }
