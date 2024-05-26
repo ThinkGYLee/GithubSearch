b971dd5b562da5996c909f94c737a294ea9cb03b
ThinkGYLee <ntnpholicfp@gmail.com>
2024-05-27 02:29:54 +0900

    - spotless 디펜던시 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,15 +86,15 @@ dependencies {
 
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
-    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.compose.material:material:1.6.2")
-    implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material3:material3-window-size-class:")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.3.2" apply false
@@ -6,4 +8,25 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("androidx.navigation.safeargs") version "2.7.2" apply false
+    id("com.diffplug.spotless").version("6.19.0")
+}
+
+
+subprojects {
+    plugins.apply("com.diffplug.spotless")
+
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            ktlint()
+            indentWithSpaces()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            ktlint()
+            indentWithSpaces()
+            endWithNewline()
+        }
+    }
 }
