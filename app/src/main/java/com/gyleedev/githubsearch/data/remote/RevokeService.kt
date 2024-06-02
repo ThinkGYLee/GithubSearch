eb2329c71adc6336c71d1863b0a4781d6b6c5136
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-03 04:18:54 +0900

    - RevokeService 구현

---
+++ app/src/main/java/com/gyleedev/githubsearch/data/remote/RevokeService.kt
@@ -0,0 +1,17 @@
+package com.gyleedev.githubsearch.data.remote
+
+import com.gyleedev.githubsearch.data.remote.response.RevokeResponse
+import com.gyleedev.githubsearch.domain.model.RevokeRequestBody
+import retrofit2.http.Body
+import retrofit2.http.HTTP
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+interface RevokeService {
+    @HTTP(method = "DELETE", path = "applications/{client_id}/grant", hasBody = true)
+    @Headers("Accept: application/vnd.github+json")
+    suspend fun revoke(
+        @Path("client_id") clientId: String,
+        @Body accessToken: RevokeRequestBody,
+    ): RevokeResponse
+}
