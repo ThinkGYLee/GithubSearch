a82f80a742dcd084ae4f69253e24a9dcfeae2363
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-13 21:36:17 +0900

    dependency 관련 에러 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,6 +129,10 @@ dependencies {
     implementation("com.github.skydoves:landscapist-glide:2.3.3")
     implementation("com.github.skydoves:landscapist-placeholder:2.3.3")
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    ksp("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
