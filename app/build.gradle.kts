2238614d20fa345ad39dba79a2587532819ae937
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-13 22:22:26 +0900

    - gradle 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,17 +122,16 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     ksp("com.google.dagger:hilt-android-compiler:2.48")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
 
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    ksp("com.github.bumptech.glide:compiler:4.16.0")
-    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+    implementation("com.github.skydoves:landscapist-glide:2.3.3")
+    implementation("com.github.skydoves:landscapist-placeholder:2.3.3")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.12.5")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
