ae85c23c37fb14e7a34c1d150ba17da257dfb19c
ThinkGYLee <ntnpholicfp@gmail.com>
2024-08-02 03:52:19 +0900

    - gradle의 디펜던시 전체 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-    id("androidx.navigation.safeargs") version "2.7.7" apply false
-    id("com.diffplug.spotless").version("6.19.0")
+    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.safeargs) apply false
+    alias(libs.plugins.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
 
