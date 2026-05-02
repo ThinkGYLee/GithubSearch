94fc1360b9ee2d699247a795cf1bc016fab6919b
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-03 00:57:30 +0900

    Feat(Build) : AndroidLibraryConventionPlugin 추가
    모든 안드로이드 라이브러리 모듈에서 공통으로 사용할
    SDK 및 코틀린 설정을 정의하는 컨벤션 플러그인을 추가함.

---
+++ build-logic/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,23 @@
+import com.android.build.gradle.LibraryExtension
+import com.gyleedev.build_logic.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+/**
+ * 안드로이드 라이브러리 모듈에서 공통으로 사용할 설정을 정의하는 컨벤션 플러그인입니다.
+ */
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+            }
+
+            configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 34
+            }
+        }
+    }
+}
