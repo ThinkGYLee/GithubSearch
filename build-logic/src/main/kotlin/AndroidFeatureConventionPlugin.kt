c9a92ffc3dd5b987ad2fd49d6df6871fa3fbf23d
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-03 00:57:30 +0900

    Feat(Build) : AndroidFeatureConventionPlugin 추가
    Feature 모듈 개발에 필요한 Library, Compose, Hilt,
    그리고 Navigation 설정을 통합적으로 주입하는 컨벤션 플러그인을 구현함.

---
+++ build-logic/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,34 @@
+import com.android.build.gradle.LibraryExtension
+import com.gyleedev.build_logic.configureComposeAndroid
+import com.gyleedev.build_logic.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+/**
+ * Feature 모듈에서 공통으로 사용할 설정을 정의하는 컨벤션 플러그인입니다.
+ * Library 설정, Compose 설정, Hilt 설정을 모두 포함합니다.
+ */
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("gyleedev.android.library")
+                apply("gyleedev.android.hilt")
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+
+            configure<LibraryExtension> {
+                configureComposeAndroid(this)
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("androidx-hilt-navigation-compose").get())
+                add("implementation", libs.findLibrary("navigation-compose").get())
+                add("implementation", libs.findLibrary("androidx-lifecycle-runtime-compose").get())
+                add("implementation", libs.findLibrary("androidx-lifecycle-viewmodel-compose").get())
+            }
+        }
+    }
+}
