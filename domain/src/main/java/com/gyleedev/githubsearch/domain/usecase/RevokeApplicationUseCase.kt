931dd064349f5f16edbed6a16b0b48da27e45c7b
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-04 15:02:45 +0900

    Refactor(Domain): RevokeApplicationUseCase에 토큰 삭제 로직 통합

--- domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/RevokeApplicationUseCase.kt
+++ domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/RevokeApplicationUseCase.kt
@@ -6,5 +6,6 @@ import javax.inject.Inject
 class RevokeApplicationUseCase @Inject constructor(private val repository: GitHubRepository) {
     suspend operator fun invoke() {
         repository.revokeApplication()
+        repository.deleteAccessToken()
     }
 }
