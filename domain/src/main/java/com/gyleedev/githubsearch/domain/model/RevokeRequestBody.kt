e647aae5021db78bad7e246aba93f3422c560f29
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-04 15:13:52 +0900

    Refactor(Domain): RevokeRequestBody에서 Gson 어노테이션(@SerializedName) 제거    - 도메인 모델에서 데이터 계층 전송 포맷에 대한 의존성을 제거하여 계층 간 순수성 확보

--- domain/src/main/java/com/gyleedev/githubsearch/domain/model/RevokeRequestBody.kt
+++ domain/src/main/java/com/gyleedev/githubsearch/domain/model/RevokeRequestBody.kt
@@ -1,8 +1,3 @@
 package com.gyleedev.githubsearch.domain.model
 
-import com.google.gson.annotations.SerializedName
-
-data class RevokeRequestBody(
-    @SerializedName("access_token")
-    val accessToken: String,
-)
+data class RevokeRequestBody(val accessToken: String)
