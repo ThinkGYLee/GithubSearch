7bcce93c168c48c560adc3df3ea34663a1da2d6a
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-04 15:14:41 +0900

    Refactor(Data): RevokeService가 도메인 모델 대신 데이터 DTO를 사용하도록 수정    - Retrofit 인터페이스에서 도메인 계층의 모델을 직접 사용하는 의존성 결합 문제 해결

--- data/src/main/java/com/gyleedev/data/remote/RevokeService.kt
+++ data/src/main/java/com/gyleedev/data/remote/RevokeService.kt
@@ -1,12 +1,12 @@
 package com.gyleedev.data.remote
 
+import com.gyleedev.data.remote.request.RevokeRequest
 import com.gyleedev.data.remote.response.RevokeResponse
-import com.gyleedev.githubsearch.domain.model.RevokeRequestBody
 import retrofit2.http.Body
 import retrofit2.http.HTTP
 import retrofit2.http.Path
 
 interface RevokeService {
     @HTTP(method = "DELETE", path = "applications/{client_id}/grant", hasBody = true)
-    suspend fun revoke(@Path("client_id") clientId: String, @Body accessToken: RevokeRequestBody): RevokeResponse
+    suspend fun revoke(@Path("client_id") clientId: String, @Body accessToken: RevokeRequest): RevokeResponse
 }
