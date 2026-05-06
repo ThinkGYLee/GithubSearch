10de36463782c6203bb8fb041b797fa610be27b9
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-07 02:36:31 +0900

    Refactor(Domain) FetchUserUseCase.kt   - 내부 로직 추가

--- domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/FetchUserUseCase.kt
+++ domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/FetchUserUseCase.kt
@@ -7,5 +7,15 @@ import javax.inject.Inject
 class FetchUserUseCase @Inject constructor(
     private val repository: GitHubRepository,
 ) {
-    suspend operator fun invoke(query: String): SearchStatus = repository.fetchUserFromGithub(query)
+    suspend operator fun invoke(query: String): SearchStatus = try {
+        val user = repository.fetchUser(query)
+        if (user != null) {
+            repository.insertUser(user)
+            repository.upsertAccessTime(query, false)
+        }
+        SearchStatus.SUCCESS
+    } catch (e: Exception) {
+        // TODO 에러별로 다른 스테이트 떨굴것
+        SearchStatus.BAD_NETWORK
+    }
 }
