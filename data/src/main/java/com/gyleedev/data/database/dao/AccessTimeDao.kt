93f095f75c6f361712a81d073435d38c24cd9fb2
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-06 22:27:42 +0900

    Feat(Data) : AccessTime 캐시 만료 필드 및 Room Flow 쿼리 추가
    - Repo fetch 여부를 추적하기 위한 isRepoFetched 필드 추가
    - 데이터 관찰을 위한 Flow 반환 쿼리 추가

--- data/src/main/java/com/gyleedev/data/database/dao/AccessTimeDao.kt
+++ data/src/main/java/com/gyleedev/data/database/dao/AccessTimeDao.kt
@@ -10,17 +10,17 @@ import com.gyleedev.data.database.entity.AccessTimeEntity
 @Dao
 interface AccessTimeDao {
     @Query("SELECT * FROM access_time WHERE github_id = :id  COLLATE NOCASE")
-    fun getTimeByGithubId(id: String): AccessTimeEntity?
+    suspend fun getTimeByGithubId(id: String): AccessTimeEntity?
 
     @Insert
-    fun insertTime(accessTimeEntity: AccessTimeEntity)
+    suspend fun insertTime(accessTimeEntity: AccessTimeEntity)
 
     @Delete
-    fun deleteTime(accessTimeEntity: AccessTimeEntity)
+    suspend fun deleteTime(accessTimeEntity: AccessTimeEntity)
 
     @Update
-    fun updateTime(accessTimeEntity: AccessTimeEntity)
+    suspend fun updateTime(accessTimeEntity: AccessTimeEntity)
 
     @Query("DELETE FROM access_time")
-    fun resetAccessTime()
+    suspend fun resetAccessTime()
 }
