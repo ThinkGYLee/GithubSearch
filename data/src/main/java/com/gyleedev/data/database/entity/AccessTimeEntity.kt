0f063d18a2cfbb6f56ce017e2192469841f52439
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-07 02:21:32 +0900

    Refactor(Data) AccessTimeEntity.kt   - index 추가 (github_id)

--- data/src/main/java/com/gyleedev/data/database/entity/AccessTimeEntity.kt
+++ data/src/main/java/com/gyleedev/data/database/entity/AccessTimeEntity.kt
@@ -2,10 +2,14 @@ package com.gyleedev.data.database.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.time.Instant
 
-@Entity(tableName = "access_time")
+@Entity(
+    tableName = "access_time",
+    indices = [Index(value = ["github_id"], unique = true)],
+)
 data class AccessTimeEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
