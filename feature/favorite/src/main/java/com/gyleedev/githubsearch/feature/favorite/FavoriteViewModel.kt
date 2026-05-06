c8f54d6dc01abd9f16aa0d575477cd1f94a841d1
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-06 11:15:33 +0900

    Feat(Favorite) : Filter 관련 수정  - filterstatus 관리를 뷰모델로 이관  - 복잡했던 filter 방식을 간편화  - ui 에서 분기치지 않고 state 받아서 쓰도록

--- feature/favorite/src/main/java/com/gyleedev/githubsearch/feature/favorite/FavoriteViewModel.kt
+++ feature/favorite/src/main/java/com/gyleedev/githubsearch/feature/favorite/FavoriteViewModel.kt
@@ -10,6 +10,7 @@ import com.gyleedev.githubsearch.domain.usecase.UpdateFavoriteStatusUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,30 +22,19 @@ constructor(
     private val updateFavoriteUseCase: UpdateFavoriteStatusUseCase,
     private val getFavoritesUseCase: GetFavoritesUseCase,
 ) : BaseViewModel() {
-    private val filterState = MutableStateFlow(FilterStatus.ALL)
+    private val _filterState = MutableStateFlow(FilterStatus.ALL)
+    val filterState: StateFlow<FilterStatus> = _filterState
 
     @OptIn(ExperimentalCoroutinesApi::class)
     val items =
-        filterState
+        _filterState
             .flatMapLatest {
                 getFavoritesUseCase(it)
             }.cachedIn(viewModelScope)
 
-    fun userFilterAll() {
+    fun updateFilter(status: FilterStatus) {
         viewModelScope.launch {
-            filterState.emit(FilterStatus.ALL)
-        }
-    }
-
-    fun userFilterHasRepos() {
-        viewModelScope.launch {
-            filterState.emit(FilterStatus.REPO)
-        }
-    }
-
-    fun userFilterNoRepos() {
-        viewModelScope.launch {
-            filterState.emit(FilterStatus.NOREPO)
+            _filterState.emit(status)
         }
     }
 
