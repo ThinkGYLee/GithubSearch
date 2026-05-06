16b958bdf10124bf0435f36ce6fbdf24fe429eec
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-07 02:26:31 +0900

    Refactor(Data) AccessTimeDao.kt   - insertTime 제거   - deleteTime 제거   - upsertTime 추가   - updateTime 제거

--- data/src/main/java/com/gyleedev/data/database/dao/AccessTimeDao.kt
+++ data/src/main/java/com/gyleedev/data/database/dao/AccessTimeDao.kt
@@ -1,10 +1,8 @@
 package com.gyleedev.data.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
+import androidx.room.Upsert
 import com.gyleedev.data.database.entity.AccessTimeEntity
 
 @Dao
@@ -12,14 +10,8 @@ interface AccessTimeDao {
     @Query("SELECT * FROM access_time WHERE github_id = :id  COLLATE NOCASE")
     suspend fun getTimeByGithubId(id: String): AccessTimeEntity?
 
-    @Insert
-    suspend fun insertTime(accessTimeEntity: AccessTimeEntity)
-
-    @Delete
-    suspend fun deleteTime(accessTimeEntity: AccessTimeEntity)
-
-    @Update
-    suspend fun updateTime(accessTimeEntity: AccessTimeEntity)
+    @Upsert
+    suspend fun upsertAccessTime(accessTimeEntity: AccessTimeEntity)
 
     @Query("DELETE FROM access_time")
     suspend fun resetAccessTime()
