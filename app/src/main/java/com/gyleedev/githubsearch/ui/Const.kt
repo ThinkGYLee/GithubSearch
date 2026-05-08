b6088676d2ac733b6418725d16a16ca517ae988a
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-09 00:11:07 +0900

    Fix(App) Const   - HOME 의 값 틀린것 수정   - "CALENDAR" -> "HOME"

--- app/src/main/java/com/gyleedev/githubsearch/ui/Const.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/Const.kt
@@ -1,6 +1,6 @@
 package com.gyleedev.githubsearch.ui
 
-const val HOME = "CALENDAR"
+const val HOME = "HOME"
 const val SETTING = "SETTING"
 const val DETAIL = "DETAIL"
 const val FAVORITE = "FAVORITE"
