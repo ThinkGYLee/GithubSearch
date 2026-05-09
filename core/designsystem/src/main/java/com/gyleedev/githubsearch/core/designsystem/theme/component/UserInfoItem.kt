46da9171efc587bb05c8f59c12d9372776997d4e
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-09 23:31:07 +0900

    Feature(DesignSystem) Ui 공용 컴퍼넌트 추가   - UserInfoItem

---
+++ core/designsystem/src/main/java/com/gyleedev/githubsearch/core/designsystem/theme/component/UserInfoItem.kt
@@ -0,0 +1,43 @@
+package com.gyleedev.githubsearch.core.designsystem.theme.component
+
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UserInfoItem(
+    avatar: String,
+    login: String,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .heightIn(min = 80.dp, max = 100.dp)
+                .combinedClickable(
+                    onClick = onClick,
+                    onLongClick = onLongClick,
+                )
+                .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        UserAvatar(avatar = avatar)
+        Text(
+            text = login,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
