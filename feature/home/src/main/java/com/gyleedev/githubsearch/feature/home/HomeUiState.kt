43b547a3cab7d63018dc7c076f0e53a4be8454d6
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-09 23:32:05 +0900

    Refactor(Home) HomeUiState 디벨롭   - stateHoisting 범위 확장   - SearchUiState 추가

--- feature/home/src/main/java/com/gyleedev/githubsearch/feature/home/HomeUiState.kt
+++ feature/home/src/main/java/com/gyleedev/githubsearch/feature/home/HomeUiState.kt
@@ -1,14 +1,28 @@
 package com.gyleedev.githubsearch.feature.home
 
-import com.gyleedev.githubsearch.domain.model.UserModel
+import androidx.compose.runtime.Stable
 
+@Stable
 sealed interface HomeUiState {
     data object Loading : HomeUiState
 
     data class Success(
         val searchQuery: String,
-        val searchedUser: UserModel?,
         val isLoading: Boolean,
-
+        val searchState: SearchUiState,
+        val isSearchActive: Boolean,
+        val showRequestAuthDialog: Boolean,
     ) : HomeUiState
+}
+
+@Stable
+sealed interface SearchUiState {
+    data object Empty : SearchUiState
+
+    data class Success(
+        val name: String?,
+        val login: String,
+        val bio: String?,
+        val avatar: String,
+    ) : SearchUiState
 }
