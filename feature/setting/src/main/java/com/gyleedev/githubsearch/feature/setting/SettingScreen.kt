e8f29d47e2bd76aa967a8132825717f16c72e31e
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-12 20:48:59 +0900

    Feature(Setting) : SettingScreen.kt   - Revoke 결과를 SanckBar 로 보여주는 처리 추가

--- feature/setting/src/main/java/com/gyleedev/githubsearch/feature/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/gyleedev/githubsearch/feature/setting/SettingScreen.kt
@@ -13,16 +13,25 @@ import androidx.compose.material.icons.outlined.Language
 import androidx.compose.material.icons.outlined.Storage
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.core.os.LocaleListCompat
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import com.gyleedev.githubsearch.domain.model.RevokeResult
 import com.gyleedev.githubsearch.feature.setting.component.RadioButtonDialog
 import com.gyleedev.githubsearch.feature.setting.component.SettingMainBlock
 import com.gyleedev.githubsearch.feature.setting.component.TwoButtonDialog
@@ -32,6 +41,7 @@ import com.gyleedev.githubsearch.feature.setting.model.SettingEvent
 import com.gyleedev.githubsearch.feature.setting.model.SettingItem
 import com.gyleedev.githubsearch.feature.setting.model.SettingRowItem
 import com.gyleedev.githubsearch.feature.setting.model.ThemeItem
+import kotlinx.coroutines.flow.collectLatest
 import java.util.Locale
 import com.gyleedev.githubsearch.feature.setting.R as SettingR
 
@@ -46,6 +56,29 @@ fun SettingScreen(
     val themeData = remember { SettingDialogItem.Theme(themeList) }
     val languageData = remember { SettingDialogItem.Language(languageList) }
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val snackBarHostState = remember { SnackbarHostState() }
+    val context = LocalContext.current
+    val revokeSuccessMessage = stringResource(id = SettingR.string.revoke_success_message)
+    val revokeFailMessage = stringResource(id = SettingR.string.revoke_fail_message)
+    val revokeNoKeyMessage = stringResource(id = SettingR.string.revoke_no_item_message)
+
+    LaunchedEffect(viewModel.showRevokeResult, lifecycleOwner, context) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.showRevokeResult.collectLatest { result ->
+                val message = when (result) {
+                    RevokeResult.SUCCESS -> revokeSuccessMessage
+                    RevokeResult.FAIL -> revokeFailMessage
+                    RevokeResult.NO_KEY -> revokeNoKeyMessage
+                }
+
+                snackBarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Short,
+                )
+            }
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -54,6 +87,7 @@ fun SettingScreen(
                 modifier = Modifier,
             )
         },
+        snackbarHost = { SnackbarHost(snackBarHostState) },
         modifier = modifier.fillMaxSize(),
     ) { paddingValues ->
 
@@ -120,7 +154,7 @@ fun SettingScreen(
                 TwoButtonDialog(
                     onDismissRequest = { viewModel.changDialogState(SettingEvent.LOGOUT) },
                     onEventRequest = {
-                        viewModel.deleteKey()
+                        viewModel.revokeApplication()
                         viewModel.changDialogState(SettingEvent.LOGOUT)
                     },
                     modifier = Modifier,
