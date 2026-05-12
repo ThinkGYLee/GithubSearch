3da6b66a63b86438bd31be426c2639673021fa2b
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-12 20:41:04 +0900

    Refactor(Data) : RevokeRequest.kt   - 필요없는 RevokeRequestBody.toRequest() 제거

--- data/src/main/java/com/gyleedev/data/remote/request/RevokeRequest.kt
+++ data/src/main/java/com/gyleedev/data/remote/request/RevokeRequest.kt
@@ -1,13 +1,8 @@
 package com.gyleedev.data.remote.request
 
 import com.google.gson.annotations.SerializedName
-import com.gyleedev.githubsearch.domain.model.RevokeRequestBody
 
 data class RevokeRequest(
     @SerializedName("access_token")
     val accessToken: String,
-)
-
-fun RevokeRequestBody.toRequest(): RevokeRequest = RevokeRequest(
-    accessToken = accessToken,
 )
