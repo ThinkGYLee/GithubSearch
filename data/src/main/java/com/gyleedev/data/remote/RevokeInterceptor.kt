5d016892b29ba44178ea82efc9023b5d9c21e0d3
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-12 20:42:25 +0900

    Refactor(Data) : RevokeInterceptor.kt   - encodedBasic 의 방식을 직접 인코딩에서 okhttp3.Credentials 를 사용하도록 수정

--- data/src/main/java/com/gyleedev/data/remote/RevokeInterceptor.kt
+++ data/src/main/java/com/gyleedev/data/remote/RevokeInterceptor.kt
@@ -3,19 +3,18 @@ package com.gyleedev.data.remote
 import com.gyleedev.data.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
-import java.util.Base64
 import javax.inject.Inject
 
 class RevokeInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val encodedBasic: String =
-            Base64
-                .getEncoder()
-                .encodeToString(("${BuildConfig.CLIENT_ID}:${BuildConfig.CLIENT_SECRET}").toByteArray())
+        val encodedBasic = okhttp3.Credentials.basic(
+            BuildConfig.CLIENT_ID,
+            BuildConfig.CLIENT_SECRET,
+        )
         val builder = chain.request().newBuilder()
 
         builder.addHeader("Accept", "application/vnd.github+json")
-        builder.addHeader("Authorization", "Basic $encodedBasic")
+        builder.addHeader("Authorization", encodedBasic)
         return chain.proceed(builder.build())
     }
 }
