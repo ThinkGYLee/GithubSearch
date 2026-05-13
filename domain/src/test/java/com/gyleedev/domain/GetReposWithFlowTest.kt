6c12bd3c69b167413f44f99ef5821b1678ed2063
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-13 22:06:27 +0900

    Refactor(Domain) : GetReposWithFlowTest   - repository 가 있을 때, 없을 때, exception 이 날 때 의 시나리오 별 테스트 작성

---
+++ domain/src/test/java/com/gyleedev/domain/GetReposWithFlowTest.kt
@@ -0,0 +1,80 @@
+package com.gyleedev.domain
+
+import com.gyleedev.githubsearch.domain.model.RepositoryModel
+import com.gyleedev.githubsearch.domain.repository.GitHubRepository
+import com.gyleedev.githubsearch.domain.usecase.GetReposWithFlowUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class GetReposWithFlowTest {
+    private val repository: GitHubRepository = mockk()
+    private lateinit var useCase: GetReposWithFlowUseCase
+    private lateinit var mockRepos: MutableList<RepositoryModel>
+
+    @Before
+    fun setUp() {
+        useCase = GetReposWithFlowUseCase(repository)
+        mockRepos = mutableListOf()
+        var count = 100L
+        while (count >= 0) {
+            mockRepos.add(
+                RepositoryModel(
+                    name = "name_$count",
+                    userGithubId = "userGithubId_${count / 4}",
+                    description = "description_$count",
+                    language = "language_$count",
+                    stargazer = count.toInt(),
+                    favorite = false,
+                ),
+            )
+            count--
+        }
+    }
+
+    @Test
+    fun `repo item 이 존재할 때`() = runTest {
+        val userId = "userGithubId_0L"
+        val expectedResult = mockRepos.filter { it.userGithubId == userId }
+
+        coEvery { repository.getReposWithFlow(userId) } returns flowOf(expectedResult)
+
+        val actual = useCase(userId)
+        assertEquals(expectedResult, actual.first())
+
+        coVerify(exactly = 1) { repository.getReposWithFlow(userId).ignoreUnused() }
+    }
+
+    @Test
+    fun `repo item 이 없을 때`() = runTest {
+        val userId = "userGithubId_0L"
+        val expectedResult = emptyList<RepositoryModel>()
+
+        coEvery { repository.getReposWithFlow(userId) } returns flowOf(expectedResult)
+
+        val actual = useCase(userId)
+        assertEquals(expectedResult, actual.first())
+
+        coVerify(exactly = 1) { repository.getReposWithFlow(userId).ignoreUnused() }
+    }
+
+    @Test
+    fun `Exception 이 날 때`() = runTest {
+        val userId = "userGithubId_0L"
+        val exception = Exception("Unknown Exception")
+        val expect = emptyList<RepositoryModel>()
+
+        coEvery { repository.getReposWithFlow(userId) } throws exception
+
+        val actual = useCase(userId)
+        assertEquals(expect, actual.first())
+
+        coVerify(exactly = 1) { repository.getReposWithFlow(userId).ignoreUnused() }
+    }
+}
