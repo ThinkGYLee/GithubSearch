dc8d44b362b87d427dd1d9480d33b2b9802b64b1
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-13 22:24:32 +0900

    Refactor(Domain) : GetUserWithFlowUseCase.kt   - try-catch 를 통해서 exception 에 대한 대응 추가

--- domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/GetUserWithFlowUseCase.kt
+++ domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/GetUserWithFlowUseCase.kt
@@ -3,10 +3,15 @@ package com.gyleedev.githubsearch.domain.usecase
 import com.gyleedev.githubsearch.domain.model.UserModel
 import com.gyleedev.githubsearch.domain.repository.GitHubRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetUserWithFlowUseCase @Inject constructor(
     private val repository: GitHubRepository,
 ) {
-    operator fun invoke(user: String): Flow<UserModel?> = repository.getUserWithFlow(user)
+    operator fun invoke(user: String): Flow<UserModel?> = try {
+        repository.getUserWithFlow(user)
+    } catch (e: Exception) {
+        flowOf(null)
+    }
 }
