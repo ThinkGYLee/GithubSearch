3a119676ba940eb4b87798a27cc57cbdc324cbe6
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-14 21:49:11 +0900

    Test(Domain) : GetAccessTokenTest 전수 상호작용 검증 보완

--- domain/src/test/java/com/gyleedev/domain/GetAccessTokenTest.kt
+++ domain/src/test/java/com/gyleedev/domain/GetAccessTokenTest.kt
@@ -5,7 +5,7 @@ import com.gyleedev.githubsearch.domain.model.GetAccessTokenUseCaseResult
 import com.gyleedev.githubsearch.domain.repository.GitHubRepository
 import com.gyleedev.githubsearch.domain.usecase.GetAccessTokenUseCase
 import io.mockk.coEvery
-import io.mockk.coVerifySequence
+import io.mockk.coVerify
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
@@ -27,21 +27,20 @@ class GetAccessTokenTest {
     fun `액세스 토큰 발급 및 저장 성공 시 성공 결과를 반환한다`() = runTest {
         // Given
         val code = "code"
-        val token = "token"
-        val expectedRepoResult = GetAccessTokenRepositoryResult.Success(token = token)
-        val expectedResult = GetAccessTokenUseCaseResult.Success
+        val expectedToken = "token"
+        val expectedRepoResult = GetAccessTokenRepositoryResult.Success(token = expectedToken)
+        val expectedUseCaseResult = GetAccessTokenUseCaseResult.Success
+
         coEvery { repository.getAccessToken(code) } returns expectedRepoResult
-        coEvery { repository.saveAccessToken(token) } just runs
+        coEvery { repository.saveAccessToken(expectedToken) } just runs
 
         // When
         val actualResult = useCase(code)
 
         // Then
-        assertEquals(expectedResult, actualResult)
-        coVerifySequence {
-            repository.getAccessToken(code)
-            repository.saveAccessToken(token)
-        }
+        assertEquals(expectedUseCaseResult, actualResult)
+        coVerify(exactly = 1) { repository.getAccessToken(code) }
+        coVerify(exactly = 1) { repository.saveAccessToken(expectedToken) }
     }
 
     @Test
@@ -49,44 +48,55 @@ class GetAccessTokenTest {
         // Given
         val code = "code"
         val expectedRepoResult = GetAccessTokenRepositoryResult.Fail
-        val expectedResult = GetAccessTokenUseCaseResult.Fail
+        val expectedUseCaseResult = GetAccessTokenUseCaseResult.Fail
+
         coEvery { repository.getAccessToken(code) } returns expectedRepoResult
+        coEvery { repository.saveAccessToken(any()) } just runs
 
         // When
         val actualResult = useCase(code)
 
         // Then
-        assertEquals(expectedResult, actualResult)
+        assertEquals(expectedUseCaseResult, actualResult)
+        coVerify(exactly = 1) { repository.getAccessToken(code) }
+        coVerify(exactly = 0) { repository.saveAccessToken(any()) }
     }
 
     @Test
     fun `액세스 토큰 발급 중 예외가 발생하면 실패 결과를 반환한다`() = runTest {
         // Given
         val code = "code"
-        val expectedResult = GetAccessTokenUseCaseResult.Fail
+        val expectedUseCaseResult = GetAccessTokenUseCaseResult.Fail
+
         coEvery { repository.getAccessToken(code) } throws Exception("Network Error")
+        coEvery { repository.saveAccessToken(any()) } just runs
 
         // When
         val actualResult = useCase(code)
 
         // Then
-        assertEquals(expectedResult, actualResult)
+        assertEquals(expectedUseCaseResult, actualResult)
+        coVerify(exactly = 1) { repository.getAccessToken(code) }
+        coVerify(exactly = 0) { repository.saveAccessToken(any()) }
     }
 
     @Test
     fun `액세스 토큰 저장 중 예외가 발생하면 실패 결과를 반환한다`() = runTest {
         // Given
         val code = "code"
-        val token = "token"
-        val expectedRepoResult = GetAccessTokenRepositoryResult.Success(token = token)
-        val expectedResult = GetAccessTokenUseCaseResult.Fail
+        val expectedToken = "token"
+        val expectedRepoResult = GetAccessTokenRepositoryResult.Success(token = expectedToken)
+        val expectedUseCaseResult = GetAccessTokenUseCaseResult.Fail
+
         coEvery { repository.getAccessToken(code) } returns expectedRepoResult
-        coEvery { repository.saveAccessToken(token) } throws Exception("Preference Error")
+        coEvery { repository.saveAccessToken(expectedToken) } throws Exception("Preference Error")
 
         // When
         val actualResult = useCase(code)
 
         // Then
-        assertEquals(expectedResult, actualResult)
+        assertEquals(expectedUseCaseResult, actualResult)
+        coVerify(exactly = 1) { repository.getAccessToken(code) }
+        coVerify(exactly = 1) { repository.saveAccessToken(expectedToken) }
     }
 }
