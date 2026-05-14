2496f6923c98035a526a6f2974c55c2a4ac91d0d
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-14 21:49:11 +0900

    Test(Domain) : UpdateFavoriteStatusTest 전수 상호작용 검증 보완

--- domain/src/test/java/com/gyleedev/domain/UpdateFavoriteStatusTest.kt
+++ domain/src/test/java/com/gyleedev/domain/UpdateFavoriteStatusTest.kt
@@ -25,14 +25,16 @@ class UpdateFavoriteStatusTest {
     fun `즐겨찾기 상태 변경 성공 시 성공 결과를 반환한다`() = runTest {
         // Given
         val user = createDummyUser(id = 1L, favorite = false)
-        val expected = UpdateFavoriteResult.Success
-        coEvery { repository.upsertUser(any()) } returns 1L
+        val expectedResult = UpdateFavoriteResult.Success
+        val expectedUpsertId = 1L
+
+        coEvery { repository.upsertUser(any()) } returns expectedUpsertId
 
         // When
-        val result = useCase(user)
+        val actualResult = useCase(user)
 
         // Then
-        assertEquals(expected, result)
+        assertEquals(expectedResult, actualResult)
         coVerify(exactly = 1) { repository.upsertUser(any()) }
     }
 
@@ -40,28 +42,33 @@ class UpdateFavoriteStatusTest {
     fun `업데이트된 ID가 기존과 다를 경우 실패 결과를 반환한다`() = runTest {
         // Given
         val user = createDummyUser(id = 1L, favorite = false)
-        val expected = UpdateFavoriteResult.Fail
-        coEvery { repository.upsertUser(any()) } returns 2L
+        val expectedResult = UpdateFavoriteResult.Fail
+        val expectedUpsertId = 2L
+
+        coEvery { repository.upsertUser(any()) } returns expectedUpsertId
 
         // When
-        val result = useCase(user)
+        val actualResult = useCase(user)
 
         // Then
-        assertEquals(expected, result)
+        assertEquals(expectedResult, actualResult)
+        coVerify(exactly = 1) { repository.upsertUser(any()) }
     }
 
     @Test
     fun `즐겨찾기 상태 변경 중 예외가 발생하면 실패 결과를 반환한다`() = runTest {
         // Given
         val user = createDummyUser(id = 1L, favorite = false)
-        val expected = UpdateFavoriteResult.Fail
+        val expectedResult = UpdateFavoriteResult.Fail
+
         coEvery { repository.upsertUser(any()) } throws Exception("Database Error")
 
         // When
-        val result = useCase(user)
+        val actualResult = useCase(user)
 
         // Then
-        assertEquals(expected, result)
+        assertEquals(expectedResult, actualResult)
+        coVerify(exactly = 1) { repository.upsertUser(any()) }
     }
 
     private fun createDummyUser(id: Long, favorite: Boolean): UserModel = UserModel(
