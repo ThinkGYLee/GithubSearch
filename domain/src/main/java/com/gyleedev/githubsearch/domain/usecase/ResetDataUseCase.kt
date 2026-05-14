d421062aa3ad8eb3e0bf9581342a27c5e4fe38bd
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-14 16:32:10 +0900

    Refactor(Domain) : ResetDataUseCase.kt   - 필요없는 async-await 로직 제거

--- domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/ResetDataUseCase.kt
+++ domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/ResetDataUseCase.kt
@@ -2,22 +2,14 @@ package com.gyleedev.githubsearch.domain.usecase
 
 import com.gyleedev.githubsearch.domain.model.ResetDataResult
 import com.gyleedev.githubsearch.domain.repository.GitHubRepository
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class ResetDataUseCase @Inject constructor(
     private val repository: GitHubRepository,
 ) {
     suspend operator fun invoke(): ResetDataResult = try {
-        coroutineScope {
-            val resetAccessTimeTask = async { repository.resetAccessTime() }
-            val resetReposTask = async { repository.resetRepos() }
-            val resetUserTask = async { repository.resetUser() }
-            awaitAll(resetAccessTimeTask, resetReposTask, resetUserTask)
-            ResetDataResult.Success
-        }
+        repository.resetUser()
+        ResetDataResult.Success
     } catch (e: Exception) {
         ResetDataResult.Fail
     }
