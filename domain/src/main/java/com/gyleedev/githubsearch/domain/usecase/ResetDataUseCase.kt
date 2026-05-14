51d3efd06b4af2366151ac5a6161fc2e2d4db3e2
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-14 15:48:51 +0900

    Feature(Domain) : ResetDataUseCase.kt   - return type 추가(ResetDataResult)   - try-catch 문을 통한 exception 대응   - async-await 을 통하여 3개의 task 를 병렬로 처리하도록 수정

--- domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/ResetDataUseCase.kt
+++ domain/src/main/java/com/gyleedev/githubsearch/domain/usecase/ResetDataUseCase.kt
@@ -1,16 +1,24 @@
 package com.gyleedev.githubsearch.domain.usecase
 
+import com.gyleedev.githubsearch.domain.model.ResetDataResult
 import com.gyleedev.githubsearch.domain.repository.GitHubRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class ResetDataUseCase @Inject constructor(
     private val repository: GitHubRepository,
 ) {
-    suspend operator fun invoke() {
-        withContext(Dispatchers.IO) {
-            repository.resetData()
+    suspend operator fun invoke(): ResetDataResult = try {
+        coroutineScope {
+            val resetAccessTimeTask = async { repository.resetAccessTime() }
+            val resetReposTask = async { repository.resetRepos() }
+            val resetUserTask = async { repository.resetUser() }
+            awaitAll(resetAccessTimeTask, resetReposTask, resetUserTask)
+            ResetDataResult.Success
         }
+    } catch (e: Exception) {
+        ResetDataResult.Fail
     }
 }
