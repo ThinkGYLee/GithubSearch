0e91c077c7a26fb21679c0cd4954d14a28ab7ae2
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-14 16:30:24 +0900

    Refactor(Data) : UserDao.kt   - id 로 되어있던 것 githubId 또는 github_id 로 수정하여 가독성 향상

--- data/src/main/java/com/gyleedev/data/database/dao/UserDao.kt
+++ data/src/main/java/com/gyleedev/data/database/dao/UserDao.kt
@@ -23,11 +23,11 @@ interface UserDao {
     @Query("SELECT * FROM user WHERE favorite = true AND repos =0")
     fun getUsersNonRepo(): PagingSource<Int, UserEntity>
 
-    @Query("SELECT * FROM user WHERE user_id = :id  COLLATE NOCASE")
-    suspend fun getUser(id: String): UserEntity?
+    @Query("SELECT * FROM user WHERE github_id = :githubId  COLLATE NOCASE")
+    suspend fun getUser(githubId: String): UserEntity?
 
-    @Query("SELECT * FROM user WHERE user_id = :userId COLLATE NOCASE")
-    fun getUserByGithubId(userId: String): Flow<UserEntity?>
+    @Query("SELECT * FROM user WHERE github_id = :githubId COLLATE NOCASE")
+    fun getUserByGithubId(githubId: String): Flow<UserEntity?>
 
     @Insert
     suspend fun insertUser(user: UserEntity): Long
