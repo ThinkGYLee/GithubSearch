76a9d82b97e114c977258c4f3e032e12e3375b96
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-17 01:22:18 +0900

    Test(Data) : TestUtils 에 ignoreUnused 추가

--- data/src/test/java/com/gyleedev/data/utils/TestUtils.kt
+++ data/src/test/java/com/gyleedev/data/utils/TestUtils.kt
@@ -3,6 +3,7 @@ package com.gyleedev.data.utils
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.gyleedev.data.database.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
 
 fun createDummyUserEntity(id: Long, repoCount: Int): UserEntity = UserEntity(
     id = id,
@@ -31,3 +32,9 @@ fun <K : Any, V : Any> createMockPagingSource(data: List<V>): PagingSource<K, V>
 
     override fun getRefreshKey(state: PagingState<K, V>): K? = null
 }
+
+/**
+ * coVerify 블록 내부에서 Flow를 반환하는 함수 검증 시 발생하는
+ * 'flow is constructed but unused' 경고를 억제하기 위한 확장 함수입니다.
+ */
+fun <T> Flow<T>.ignoreUnused() = Unit
