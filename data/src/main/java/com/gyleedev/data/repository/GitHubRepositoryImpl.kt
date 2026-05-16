aff3d3d6c3c8e47eb96a21ce6311abe45c28a28f
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-17 01:42:16 +0900

    Refactor(Data) : RevokeService.kt 에서 client_id 에 직접 BuildConfig.CLIENT_ID 를 넣도록 수정

--- data/src/main/java/com/gyleedev/data/repository/GitHubRepositoryImpl.kt
+++ data/src/main/java/com/gyleedev/data/repository/GitHubRepositoryImpl.kt
@@ -4,7 +4,6 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.map
-import com.gyleedev.data.BuildConfig
 import com.gyleedev.data.database.dao.AccessTimeDao
 import com.gyleedev.data.database.dao.ReposDao
 import com.gyleedev.data.database.dao.UserDao
@@ -267,10 +266,7 @@ class GitHubRepositoryImpl @Inject constructor(
         if (accessToken.isBlank()) {
             return RevokeResult.NO_KEY
         }
-        val response = revokeService.revoke(
-            clientId = BuildConfig.CLIENT_ID,
-            request = RevokeRequest(accessToken),
-        )
+        val response = revokeService.revoke(request = RevokeRequest(accessToken))
         return if (response.isSuccessful) {
             RevokeResult.SUCCESS
         } else {
