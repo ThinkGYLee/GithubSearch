2d833349bc21bd04a61affaef262ffe339220965
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-17 02:05:54 +0900

    Test(Data) : ResetUserRepositoryImplTest 구현

---
+++ data/src/test/java/com/gyleedev/data/repository/ResetUserRepositoryImplTest.kt
@@ -0,0 +1,56 @@
+package com.gyleedev.data.repository
+
+import com.gyleedev.data.database.dao.AccessTimeDao
+import com.gyleedev.data.database.dao.ReposDao
+import com.gyleedev.data.database.dao.UserDao
+import com.gyleedev.data.preference.TokenPreference
+import com.gyleedev.data.remote.AccessService
+import com.gyleedev.data.remote.GithubApiService
+import com.gyleedev.data.remote.RevokeService
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import java.time.Clock
+
+class ResetUserRepositoryImplTest {
+    private lateinit var repository: GitHubRepositoryImpl
+    private val userDao: UserDao = mockk()
+    private val reposDao: ReposDao = mockk()
+    private val accessTimeDao: AccessTimeDao = mockk()
+    private val githubApiService: GithubApiService = mockk()
+    private val accessService: AccessService = mockk()
+    private val revokeService: RevokeService = mockk()
+    private val tokenPreference: TokenPreference = mockk()
+    private val clock: Clock = mockk()
+
+    @Before
+    fun setUp() {
+        repository = GitHubRepositoryImpl(
+            userDao = userDao,
+            reposDao = reposDao,
+            accessTimeDao = accessTimeDao,
+            githubApiService = githubApiService,
+            accessService = accessService,
+            revokeService = revokeService,
+            tokenPreference = tokenPreference,
+            clock = clock,
+        )
+    }
+
+    @Test
+    fun `유저 초기화 시 UserDao의 resetUser를 호출한다`() = runTest {
+        // Given
+        coEvery { userDao.resetUser() } just runs
+
+        // When
+        repository.resetUser()
+
+        // Then
+        coVerify(exactly = 1) { userDao.resetUser() }
+    }
+}
