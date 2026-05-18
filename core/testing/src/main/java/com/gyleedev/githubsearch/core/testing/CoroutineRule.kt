b5aa6db01dc722b4212efb420e9c47b1bdff6345
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-18 20:26:18 +0900

    Refactor(Core) : CoroutineRule.kt 수정

--- core/testing/src/main/java/com/gyleedev/githubsearch/core/testing/CoroutineRule.kt
+++ core/testing/src/main/java/com/gyleedev/githubsearch/core/testing/CoroutineRule.kt
@@ -11,8 +11,7 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 /**
- * 테스트에서 사용할 디스패처들을 캡슐화하는 인터페이스입니다.
- * (현재는 테스트 모듈 내부에서만 사용됩니다.)
+ * 테스트에서 사용할 디스패처들을 제공하는 인터페이스입니다.
  */
 interface TestDispatcherProvider {
     val main: CoroutineDispatcher
@@ -23,9 +22,8 @@ interface TestDispatcherProvider {
 /**
  * 모든 디스패처가 하나의 [TestDispatcher]를 공유하도록 하는 구현체입니다.
  */
-class SharedTestDispatcherProvider
 @OptIn(ExperimentalCoroutinesApi::class)
-constructor(
+class SharedTestDispatcherProvider(
     val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestDispatcherProvider {
     override val main: CoroutineDispatcher = testDispatcher
@@ -34,14 +32,13 @@ constructor(
 }
 
 /**
- * 메인 디스패처를 교체하고, 테스트용 디스패처 제공자를 관리하는 JUnit Rule입니다.
+ * 메인 디스패처를 교체해주는 JUnit Rule입니다.
+ * NIA와 같이 UnconfinedTestDispatcher를 기본으로 사용하여 비동기 작업을 즉시 실행되게 합니다.
  */
 @OptIn(ExperimentalCoroutinesApi::class)
 class CoroutineRule(
     val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestWatcher() {
-
-    val dispatcherProvider: TestDispatcherProvider = SharedTestDispatcherProvider(testDispatcher)
 
     override fun starting(description: Description) {
         Dispatchers.setMain(testDispatcher)
