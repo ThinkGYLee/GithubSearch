07ffefabcbb85dae69e0f76aec18e56dd96e52b0
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-21 00:14:30 +0900

    Feature(Detail): UriUtil 추가   - domain url 을 간략화 하는 코드 추가

---
+++ feature/detail/src/main/java/com/gyleedev/githubsearch/feature/detail/UriUtil.kt
@@ -0,0 +1,30 @@
+package com.gyleedev.githubsearch.feature.detail
+
+import java.net.URI
+
+// 전달된 URL에서 프로토콜(http, https) 및 경로를 제외한 기본 도메인만 추출합니다.
+internal fun extractDomain(
+    url: String,
+): String {
+    try {
+        val isMissingProtocol = !url.startsWith(prefix = "http://") &&
+                !url.startsWith(prefix = "https://")
+
+        val validUrl = if (isMissingProtocol) {
+            "http://$url"
+        } else {
+            url
+        }
+
+        val uri = URI(validUrl)
+        val host = uri.host
+
+        if (host != null) {
+            return host
+        } else {
+            return url
+        }
+    } catch (e: Exception) {
+        return url
+    }
+}
