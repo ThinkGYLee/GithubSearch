1a2cb821c9746aecd90b68758bfd3f03fa3c31de
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-21 20:51:09 +0900

    Build(DesignSystem): Material Icon 추가

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.landscapist.glide)
     implementation(libs.landscapist.placeholder)
