0d30493ca5f822fb1b29ff72031ea6e08d842be3
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-21 21:06:23 +0900

    Feature(Home): HomeUiState.kt mode, selectedUsers 추가

--- feature/home/src/main/java/com/gyleedev/githubsearch/feature/home/HomeUiState.kt
+++ feature/home/src/main/java/com/gyleedev/githubsearch/feature/home/HomeUiState.kt
@@ -2,6 +2,12 @@ package com.gyleedev.githubsearch.feature.home
 
 import androidx.compose.runtime.Stable
 
+enum class HomeMode {
+    DEFAULT,
+    SEARCH,
+    SELECT,
+}
+
 @Stable
 sealed interface HomeUiState {
     data object Loading : HomeUiState
@@ -10,7 +16,8 @@ sealed interface HomeUiState {
         val searchQuery: String,
         val isLoading: Boolean,
         val searchState: SearchUiState,
-        val isSearchActive: Boolean,
+        val mode: HomeMode,
+        val selectedUsers: Set<String>,
         val showRequestAuthDialog: Boolean,
     ) : HomeUiState
 }
