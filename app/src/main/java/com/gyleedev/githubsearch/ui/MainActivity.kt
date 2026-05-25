da4af943508316d97ef10f6a711615e72ee8ad41
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-25 21:21:03 +0900

    Feature(App): MainActivity 에 installSplashScreen 추가

--- app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresExtension
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gyleedev.githubsearch.core.designsystem.theme.GithubSearchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             GithubSearchTheme {
