72d854e6dc06f81c01ad2abbdfc47120c2dbe7c0
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-25 21:05:20 +0900

    Build(App): Splash 관련 dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.material.icons)
     implementation(libs.androidx.material)
     implementation(libs.google.material)
+    implementation(libs.androidx.splash)
 
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
