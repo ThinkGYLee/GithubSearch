9a9cef8d52aca625eb0590bd6ba19d7c36a8a671
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-27 06:57:07 +0900

    Style(DesignSystem): PulsingHeart.kt 안쓰는 임포트 수정

--- core/designsystem/src/main/java/com/gyleedev/githubsearch/core/designsystem/theme/component/PulsingHeart.kt
+++ core/designsystem/src/main/java/com/gyleedev/githubsearch/core/designsystem/theme/component/PulsingHeart.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
