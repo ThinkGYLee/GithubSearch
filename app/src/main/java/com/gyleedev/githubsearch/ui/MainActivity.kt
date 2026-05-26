74e7ecbc0dc1bc113c3339ecd215ff56b5d3d6ef
ThinkGYLee <ntnpholicfp@gmail.com>
2026-05-26 23:22:13 +0900

    Feature(App): MainActivity Splash delay 적용   - splash 가 아예 안보이는 이슈가 있어서 강제 적용

--- app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/gyleedev/githubsearch/ui/MainActivity.kt
@@ -7,15 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresExtension
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.gyleedev.githubsearch.core.designsystem.theme.GithubSearchTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+
+        var isSplashReady = false
+        lifecycleScope.launch {
+            delay(1500L)
+            isSplashReady = true
+        }
+
+        splashScreen.setKeepOnScreenCondition { !isSplashReady }
+
         enableEdgeToEdge()
         setContent {
             GithubSearchTheme {
